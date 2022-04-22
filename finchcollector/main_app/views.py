2d8ee4738df11db94651ca519fb73f72daf86a28
sberger94 <sberger94@gmail.com>
2022-04-22 14:38:48 -0700

    Setup about page

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello ya filthy bird</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Finch Collector</h1>')
