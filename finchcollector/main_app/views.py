6e68bf76e8f8d83b85e66b6c9450805eb0a7889b
sberger94 <sberger94@gmail.com>
2022-04-25 14:50:43 -0700

    Added finch model, updated index page

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Finch
 # Create your views here.
 
-# SIMULATED DB ---- DELETE THIS LATER
-class Finch:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged cat', 4)
-]
-# -----------------------------------
 
 def home(request):
     return HttpResponse('<h1>Hello ya filthy bird</h1>')
@@ -24,4 +11,5 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
