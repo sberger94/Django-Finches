0ef82ddf6d1b165a0a185ccbf1d079b3071f63d0
sberger94 <sberger94@gmail.com>
2022-04-27 10:04:19 -0700

    Added spotted model

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+SPOTTED = (
+    ('N', 'North America'),
+    ('S', 'South America'),
+    ('A', 'Africa'),
+    ('E', 'Europe'),
+    ('S', 'Asia'),
+    ('U', 'Australia'),
+    ('T', 'Antarctica'),
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -13,3 +23,16 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+
+class Spotted(models.Model):
+    date = models.DateField()
+    location = models.CharField(
+        max_length=1,
+        choices=SPOTTED,
+        default=SPOTTED[0][0]
+        )
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Spotted in {self.get_location_display()} on {self.date}"
